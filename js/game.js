9bb89887c284d5755bf96e0de18eb64adf2cb149
TheTrabin <beyondtears@gmail.com>
2020-05-29 21:31:44 -0700

    Initial. Set items, audio, and interaction

--- js/game.js
+++ js/game.js
@@ -6,12 +6,84 @@ gameScene.init = function() {}
 
 // load asset files for our game
 gameScene.preload = function() {
+  //load images
+this.load.image('background', 'assets/images/background-city.png');
+this.load.image('building', 'assets/images/building.png');
+this.load.image('car', 'assets/images/car.png');
+this.load.image('house', 'assets/images/house.png');
+this.load.image('tree', 'assets/images/tree.png');
+
+//load sound - freesound.org, audiojungle.net, Audacity.org
+this.load.audio('treeAudio', 'assets/audio/arbol.mp3');
+this.load.audio('carAudio', 'assets/audio/auto.mp3');
+this.load.audio('houseAudio', 'assets/audio/casa.mp3');
+this.load.audio('buildingAudio', 'assets/audio/edificio.mp3');
+this.load.audio('correct', 'assets/audio/correct.mp3');
+this.load.audio('wrong', 'assets/audio/wrong.mp3');
 
 };
 
 // executed once, after assets were loaded
 gameScene.create = function() {
+//load background
 
+
+//group
+this.items = this.add.group([
+  {
+  key: 'building',
+  setXY: {
+    x:100,
+    y:240
+  }
+},
+{
+  key: 'house',
+  setXY: {
+    x:240,
+    y:280
+  },
+  setScale: {
+    x: 0.8,
+    y: 0.8,
+  }
+},
+{
+  key: 'car',
+  setXY: {
+    x:400,
+    y:300
+  },
+  setScale: {
+    x: 0.8,
+    y: 0.8,
+  }
+},
+{
+  key: 'tree',
+  setXY: {
+    x:550,
+    y:250
+  },
+  setScale: {
+    x: 0.8,
+    y: 0.8,
+  }
+},
+]);
+let bg = this.add.sprite(0,0, 'background').setOrigin(0,0);
+// items on top
+this.items.setDepth(1);
+
+
+
+Phaser.Actions.Call(this.items.getChildren(), function(item){
+  item.setInteractive();
+
+  item.on('pointerdown', function(pointer) {
+    console.log('you clicked ' + item.texture.key);
+  });
+}, this);
 
 };
 
